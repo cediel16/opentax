e909d44f449f03c7b9f5953986607d7aef87073f
Johel Cediel <cediel16@gmail.com>
2013-05-19 05:49:23 -0430

    Inicio de opentax

---
+++ form.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <link href="assets/css/bootstrap-responsive.css" rel="stylesheet"> 
+        <link href="assets/css/style.css" rel="stylesheet">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <div class="navbar navbar-fixed-top">
+                <div class="container-fluid banner">
+                    <div class="row-fluidd">
+                        <div class="pull-left">
+                            12 de enero de 2013
+                        </div>
+                        <div class="pull-right">
+                            Johel Cediel
+                        </div>
+                    </div>
+                </div>
+                <div class="navbar-inner">
+                    <div class="container">
+                        <a data-target=".navbar-responsive-collapse" data-toggle="collapse" class="btn btn-navbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </a>
+                        <a href="#" class="brand">Inicio</a>
+                        <div class="nav-collapse collapse navbar-responsive-collapse">
+                            <ul class="nav">
+                                <li><a href="#">Contribuyentes</a></li>
+                                <li><a href="#">Patentes</a></li>
+                                <li class="dropdown">
+                                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Recaudaciones <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Patente de Industria y Comercio</a></li>
+                                        <li><a href="#">Agentes de Retención</a></li>
+                                        <li><a href="#">Patente de Vehículos</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Tasas</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Liquidaciones<b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Patente de Industria y Comercio</a></li>
+                                        <li><a href="#">Agentes de Retención</a></li>
+                                        <li><a href="#">Patente de Vehículos</a></li>
+                                        <li><a href="#">Inmuebles Urbanos</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Tasas</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <ul class="nav pull-right">
+                                <li class="divider-vertical"></li>
+                                <li class="dropdown">
+                                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Caja <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Action</a></li>
+                                        <li><a href="#">Another action</a></li>
+                                        <li><a href="#">Something else here</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div><!-- /.nav-collapse -->
+                    </div>
+                </div><!-- /navbar-inner -->
+            </div>
+        </header>
+        <section class="container main-container">
+            <div class="main-title">
+                Registro de...
+            </div>
+            <div class="main-content">
+                <legend>INFORMACIÓN OTORGADA POR LA DIRECCIÓN DE HACIENDA </legend>
+                <div class="row-fluid">
+                    <div class="span12">
+                        <div class="row-fluid">   
+                            <div class="span6 lightblue">
+                                <label>Número de patente aprobada</label>
+                                <input type="text" class="span12">
+                            </div><!--/span-->
+                            <div class="span6 lightblue">
+                                <label>Número de patente aprobada</label>
+                                <input type="text" class="span12">
+                            </div><!--/span-->
+                        </div><!--/row-->
+                        <div class="row-fluid">
+                            <div class="span12 lightblue">
+                                <label>Observaciones</label>
+                                <textarea class="span12"></textarea>  
+                            </div><!--/span-->
+                        </div><!--/row-->
+                    </div><!--/span-->
+                </div><!--/row-->
+                <hr>
+                <legend>DATOS DEL FONDO DE COMERCIO </legend>
+                <div class="row-fluid">
+                    <div class="span12">
+                        <div class="row-fluid">   
+                            <div class="span6 lightblue">
+                                <label>RIF</label>
+                                <input type="text" class="span12">
+                            </div><!--/span-->
+                            <div class="span6 lightblue">
+                                <label>Razón Social</label>
+                                <input type="text" class="span12">
+                            </div><!--/span-->
+                        </div><!--/row-->
+                        <div class="row-fluid">
+                            <div class="span12 lightblue">
+                                <label>Domicilio fiscal</label>
+                                <textarea class="span12"></textarea>  
+                            </div><!--/span-->
+                        </div><!--/row-->
+                    </div><!--/span-->
+                </div><!--/row-->
+                <div class="row-fluid">
+                    <div class="span12">
+                        <div class="row-fluid">
+                            <div class="span4 lightblue">
+                                <label>Estado</label>
+                                <select class="span12"></select>
+                            </div><!--/span-->
+                            <div class="span4 lightblue">
+                                <label>Municipio</label>
+                                <select class="span12"></select>
+                            </div><!--/span-->
+                            <div class="span4 lightblue">
+                                <label>Parroquia</label>
+                                <select class="span12"></select>
+                            </div><!--/span-->
+                        </div><!--/row-->
+                        <div class="row-fluid">
+                            <div class="span4 lightblue">
+                                <label>Correo electrónico</label>
+                                <input type="text" class="span12" />
+                            </div><!--/span-->
+                            <div class="span4 lightblue">
+                                <label>Teléfono</label>
+                                <input type="text" class="span12" />
+                            </div><!--/span-->
+                            <div class="span4 lightblue">
+                                <label>Fax</label>
+                                <input type="text" class="span12" />
+                            </div><!--/span-->
+                        </div><!--/row-->
+                        <div class="row-fluid">
+                            <div class="span4 lightblue">
+                                Datos del ejercicio económico 
+                            </div><!--/span-->
+                            <div class="span4 lightblue">
+                                <label>Fecha de inicio</label>
+                                <input type="text" class="span12" />
+                            </div><!--/span-->
+                            <div class="span4 lightblue">
+                                <label>Fecha de cierre</label>
+                                <input type="text" class="span12" />
+                            </div><!--/span-->
+                        </div><!--/row-->
+                    </div><!--/span-->
+                </div><!--/row-->
+                <legend>ACTIVIDADES ECONÓMICAS</legend>
+                <div class="row-fluid">
+                </div><!--/row-->
+                <hr>
+            </div>
+        </section>
+
+        <footer>
+        </footer>
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <link href="assets/css/bootstrap-responsive.css" rel="stylesheet"> 
+        <link href="assets/css/style.css" rel="stylesheet">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <div class="navbar navbar-fixed-top">
+                <div class="container-fluid banner">
+                    <div class="row-fluidd">
+                        <div class="pull-left">
+                            12 de enero de 2013
+                        </div>
+                        <div class="pull-right">
+                            Johel Cediel
+                        </div>
+                    </div>
+                </div>
+                <div class="navbar-inner">
+                    <div class="container">
+                        <a data-target=".navbar-responsive-collapse" data-toggle="collapse" class="btn btn-navbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </a>
+                        <a href="#" class="brand">Inicio</a>
+                        <div class="nav-collapse collapse navbar-responsive-collapse">
+                            <ul class="nav">
+                                <li><a href="#">Contribuyentes</a></li>
+                                <li><a href="#">Patentes</a></li>
+                                <li class="dropdown">
+                                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Recaudaciones <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Patente de Industria y Comercio</a></li>
+                                        <li><a href="#">Agentes de Retención</a></li>
+                                        <li><a href="#">Patente de Vehículos</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Tasas</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Liquidaciones<b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Patente de Industria y Comercio</a></li>
+                                        <li><a href="#">Agentes de Retención</a></li>
+                                        <li><a href="#">Patente de Vehículos</a></li>
+                                        <li><a href="#">Inmuebles Urbanos</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Tasas</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <ul class="nav pull-right">
+                                <li class="divider-vertical"></li>
+                                <li class="dropdown">
+                                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Caja <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Action</a></li>
+                                        <li><a href="#">Another action</a></li>
+                                        <li><a href="#">Something else here</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div><!-- /.nav-collapse -->
+                    </div>
+                </div><!-- /navbar-inner -->
+            </div>
+        </header>
+        <section class="container main-container">
+            <div class="main-title">
+                Titulo
+            </div>
+            <div class="main-content">
+                ad
+            </div>
+        </section>
+        <footer>
+        </footer>
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+    </body>
+</html>
